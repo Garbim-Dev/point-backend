f716f0cc3f25b3b0095388d2246a771ddf3d0072
Seu Nome Aqui <garbim@hotmail.com>
2026-05-12 16:36:07 -0300

    Configurando PostgreSQL e requirements

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # ATENÇÃO: Substitua 'postgres' pelo seu usuário e 'suasenha' pela sua senha do PostgreSQL
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:garbim@localhost/point_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://point_db_user:3k0piyFxbhKJSuJ8LSUXxdosSwVt08zk@dpg-d81nrtf7f7vs73bn4png-a.ohio-postgres.render.com/point_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
